17b825abff8ef5b4bf5c6d04a58e1583e0882b00
Oleksandr Rudenko <alexander.rudenko@gmail.com>
2014-04-14 23:56:14 +0800

    Fixed copying files to nginx

--- run-tests.sh
+++ run-tests.sh
@@ -5,9 +5,7 @@ export DISPLAY=:99
 
 grunt test
 
-# python -m SimpleHTTPServer
-# /usr/share/nginx/www
-cp src /usr/share/nginx/www/src
+cp -avr src /usr/share/nginx/www/src
 
 cd func_tests
 ./runtests.py

20126d7e1b327f4f00739f688e4f8b18724e0b57
Kevin Redman <iredmedia@gmail.com>
2014-05-28 22:12:16 -0400

    Cleanup test runner

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,3 @@
-sudo /etc/init.d/xvfb start
-
 # Now we have to set the DISPLAY env variable so Firefox and Chrome know where to open the browser.
 export DISPLAY=:99
 
@@ -7,8 +5,6 @@ grunt test
 
 cp -avr src /usr/share/nginx/www/src
 cp -avr bower_components /usr/share/nginx/www/bower_components
-# cp -avr libs /usr/share/nginx/www/libs
-# cp -avr jsx /usr/share/nginx/www/jsx
 
 cd func_tests
 ./runtests.py
